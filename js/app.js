2d4a2a3642bb6a8c4c6ce8ceb896ab8c30412e7e
Don Pham <wrathofdon@gmail.com>
2017-06-13 13:42:19 -0700

    constructor done

--- js/app.js
+++ js/app.js
@@ -1,74 +1,34 @@
 'use strict';
 
-var pike = {
-  name: '1st and Pike',
-  image: 'https://c2.staticflickr.com/4/3282/5836876205_38b0b01a78_n.jpg',
-  image_link: 'https://www.flickr.com/photos/rickvaughn/5836876205/sizes/l',
-  caption: 'Some rights reserved by saipanrick',
-  min: 23,
-  max: 65,
-  ave: 6.3,
-  storeLog: [],
-  customersPerHour : function() {
-    return(Math.floor((Math.random() * (this.max + 1 - this.min) + this.min) * this.ave));
-  }
+// array of all the stores
+var stores = [];
+
+function Store (name, min, max, ave) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.ave = ave;
+  // savedLog will be set at the end
+  this.savedLog = [];
+  // newDay is a function that will reset the savedLog
+  this.newDay = function() {
+    this.savedLog = [];
+    for (var i = 0; i < 15; i++) {
+      this.savedLog.push(Math.round((Math.random() * (this.max - this.min) + this.min) * this.ave));
+    }
+    return(this.savedLog);
+  };
+  this.newDay();
+  stores.push(this);
 };
 
-var seatac = {
-  name: 'SeaTac Airport',
-  image: 'https://c2.staticflickr.com/6/5454/6903623418_b84631d6b2_n.jpg',
-  image_link: 'https://www.flickr.com/photos/avgeekjoe/6903623418/sizes/l',
-  caption: 'Some rights reserved by AvgeekJoe',
-  min: 3,
-  max: 24,
-  ave: 1.2,
-  storeLog: [],
-  customersPerHour : function() {
-    return(Math.floor((Math.random() * (this.max + 1 - this.min) + this.min) * this.ave));
-  }
-};
+var pike = new Store('1st and Pike', 23, 65, 6.3);
+var seatac = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capitol = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
 
-var seattleCenter = {
-  name: 'Seattle Center',
-  image: 'https://c1.staticflickr.com/3/2048/2167825832_5ea784c8b5_n.jpg',
-  image_link: 'https://www.flickr.com/photos/sean_oneill/2167825832/sizes/l',
-  caption: 'Some rights reserved by Shutterbug Fotos',
-  min: 11,
-  max: 38,
-  ave: 3.7,
-  storeLog: [],
-  customersPerHour : function() {
-    return(Math.floor((Math.random() * (this.max + 1 - this.min) + this.min) * this.ave));
-  }
-};
 
-var capitol = {
-  name: 'Capitol Hill',
-  image: 'https://c2.staticflickr.com/6/5521/9672770640_7b72c38a62_n.jpg',
-  image_link: 'https://www.flickr.com/photos/wiredforsound23/9672770640/sizes/l',
-  caption: ' Some rights reserved by wiredforlego',
-  min: 20,
-  max: 38,
-  ave: 2.3,
-  storeLog: [],
-  customersPerHour : function() {
-    return(Math.floor((Math.random() * (this.max + 1 - this.min) + this.min) * this.ave));
-  }
-};
-
-var alki = {
-  name: 'Alki',
-  image: 'https://c2.staticflickr.com/4/3338/3553891900_f6775d5484_n.jpg',
-  image_link: 'https://www.flickr.com/photos/dcoetzee/3553891900/sizes/l',
-  caption: 'Some rights reserved by D Coetzee',
-  min: 2,
-  max: 16,
-  ave: 4.6,
-  storeLog: [],
-  customersPerHour : function() {
-    return(Math.round((Math.random() * (this.max + 1 - this.min) + this.min) * this.ave));
-  }
-};
 
 function predictDay(store) {
   var hour = 6;
