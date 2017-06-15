c1fb5f1e5fa8a552fcff2f6ab38be16a8e8685d3
Don Pham <wrathofdon@gmail.com>
2017-06-14 17:17:55 -0700

    Added some table tags

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // array of all the stores, will be added via construction
-var stores = [];
 var storeNames = [];
+var tbody;
 var times = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Location Total:'];
 
 // ignore the linter issues, functions are called via for loop
@@ -11,24 +11,27 @@ var seatac = new Store('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capitol = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
-
+var stores = [pike, seatac, seattleCenter, capitol, alki];
 // calculates the maximum number of cookies sold per day
 var storeMax = 0;
 var totalMax = 0;
 
-
-function Store (location, min, max, ave) {
-  this.location = location;
-  storeNames.push(this.location.toLowerCase());
+function Store (name, min, max, ave) {
+  this.name = name;
+  storeNames.push(this.name.toLowerCase());
   this.min = min;
   this.max = max;
   this.ave = ave;
+  // totalMax += this.max * this.ave;
+  // if (this.max * this.ave > storeMax) {
+  //   storeMax = (this.max * this.ave);
+  // }
   // savedLog will be set at the end
   this.savedLog = [];
   // newDay is a function that can reset the savedLog if necessary
   this.newDay = function() {
     var total = 0;
-    this.savedLog = [this.location];
+    this.savedLog = [this.name];
     for (var i = 0; i < 14; i++) {
       var sale = Math.round((Math.random() * (this.max - this.min) + this.min) * this.ave);
       this.savedLog.push(sale);
@@ -39,11 +42,9 @@ function Store (location, min, max, ave) {
   };
   this.newDay();
   // store is added to array on line 4
-  stores.push(this);
 };
 
-
-Store.prototype.render = function(table, maximum) {
+Store.prototype.render = function(tbody, maximum) {
   var row = document.createElement('tr');
   for (var i = 0; i < times.length; i++) {
     var cell = document.createElement('td');
@@ -70,7 +71,7 @@ Store.prototype.render = function(table, maximum) {
     }
     row.appendChild(cell);
   }
-  table.appendChild(row);
+  tbody.appendChild(row);
 };
 
 
@@ -98,36 +99,37 @@ var table = document.createElement('table');
 generateHeader(times, table);
 
 // renders individual stores
+tbody = document.createElement('tbody');
 for (i = 0; i < stores.length; i++) {
-  stores[i].render(table, storeMax);
+  stores[i].render(tbody, storeMax);
 };
+table.appendChild(tbody);
 
 //renders total
 generateFooter(table, totalMax);
-
 article.appendChild(table);
 
 //converts array into table row
 function generateHeader(rowArray, table) {
+  var thead = document.createElement('thead');
   var row = document.createElement('tr');
   for (var i = 0; i < times.length; i++) {
     var cell = document.createElement('th');
     cell.textContent = rowArray[i];
-    row.appendChild(cell);
-  }
-  table.appendChild(row);
+    row.appendChild(cell);}
+  thead.appendChild(row);
+  table.appendChild(thead);
 };
 
 //converts array into table row
 function generateFooter(table, maximum) {
+  var tfoot = document.createElement('tfoot');
   //generates an array with the totals for any given hour
   var totals = ['Totals: '];
   for (i = 1; i < 16; i++) {
     totals.push(0);
     for (var j = 0; j < stores.length; j++) {
-      totals[i] += stores[j].savedLog[i];
-    }
-  }
+      totals[i] += stores[j].savedLog[i];}}
   var row = document.createElement('tr');
   for (var i = 0; i < times.length; i++) {
     var cell = document.createElement('td');
@@ -148,28 +150,32 @@ function generateFooter(table, maximum) {
     }
     row.appendChild(cell);
   }
-  table.appendChild(row);
+  tfoot.appendChild(row);
+  table.appendChild(tfoot);
 };
 
 
 var addStore = document.getElementById('addStore');
-// event listeners ened to know: what event do they care about, and what do they want to do when it happens.
+
+
 addStore.addEventListener('submit',
   function (event) {
+    event.preventDefault();
     var location = event.target.location.value;
     var minCust = event.target.minCust.value;
     var maxCust = event.target.maxCust.value;
-    var aveCookies = parseFloat(event.target.aveCookies.value);
+    var aveCookies = event.target.aveCookies.value;
     if (storeNames.indexOf(location.toLowerCase()) > -1) {
-      event.preventDefault();
       alert('That\'s already a store!');
     } else if (minCust > maxCust) {
-      event.preventDefault();
-      alert('You minimum and maximum might be backwards!');
+      alert('Your minimum is greater than your maximum!');
+    } else if (minCust < 0 || aveCookies < 0) {
+      alert('You cannot have negative numbers');
+    } else if (maxCust == 0 && aveCookies == 0) {
+      alert('Imagine that you have zero cookies and you split them evenly among zero friends. How many cookies does each person get\? See\? It doesn\'t make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends.');
     } else {
-      event.preventDefault();
       var newStore = new Store(location, minCust, maxCust, aveCookies);
-      newStore.render();
+      newStore.render(tbody, storeMax);
       addStore.reset();
     }
   }
